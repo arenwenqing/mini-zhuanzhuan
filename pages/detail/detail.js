786b4e47a46ef15e0fa69d3e2c1778f8b07e0139
zhoushanlin <zhoushanlin@inke.cn>
2021-11-04 21:12:26 +0800

    拉取main

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,4 +1,5 @@
 // pages/detail/detail.js
+import API from '../../service/apis'
 const domain = 'https://tuanzhzh.com'
 Page({
 
@@ -8,7 +9,8 @@ Page({
   data: {
     detailData: {},
     carousel: [],
-    title: ''
+    title: '',
+    productId: ''
   },
 
   /**
@@ -30,10 +32,12 @@ Page({
       url: domain + `/mini/product/detail/${id}`,
       method: 'GET',
       success: (res) => {
+        debugger
         res.data.data.price = (res.data.data.price / 100).toFixed(2)
         this.setData({
           detailData: res.data.data ? res.data.data : {},
-          carousel: res.data.data.photoAddress ? [res.data.data.photoAddress.shift()]: []
+          carousel: res.data.data.photoAddress ? [res.data.data.photoAddress.shift()]: [],
+          productId: res.data?.data?.productId
         })
       },
       fail: (err) => {
@@ -46,6 +50,49 @@ Page({
       complete: () => {
         wx.hideLoading()
       }
+    })
+  },
+
+  // 用劵拼团
+  useCouponsSpellGroup(e) {
+    this.submitProductGetOrderId(this.data.productId, 1)
+  },
+  // 立即拼团
+  immediateSpellGroup(e) {
+    this.submitProductGetOrderId(this.data.productId, 0)
+  },
+
+  // 提交订单获取订单id(orderid)
+  // isUseRoll 是否用劵， 1用劵，0非用劵
+  submitProductGetOrderId(productId, isUseRoll) {
+    API.sumbitProduct({
+      productId,
+      doubleQuotaCount: isUseRoll,
+      payUserId: wx.getStorageSync('userId') || '',
+      receiveAddressId: wx.getStorageSync('addressId') || ''
+    }).then(res => {
+      const data = res.data.data
+      if (data.submitSuccess) { // 提交订单成功
+        console.log('提交订单失败', data.submitMsg)
+        const orderId = data?.order?.orderId || ''
+        // 跳转订单详情页
+        wx.navigateTo({
+          url: '/pages/orderDetail/orderDetail?orderId=' + orderId
+        })
+      } else { // 提交订单失败
+        console.log('提交订单失败', data.submitMsg)
+        wx.showToast({
+          title: data.submitMsg,
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    }).catch(err => {
+      wx.showToast({
+        title: err.data.msg,
+        icon: 'error',
+        duration: 2000
+      })
     })
   },
 
